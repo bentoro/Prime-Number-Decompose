0f81481e67654e8cf60c24fb9d195738998e9cf2
Ben <benedict.sh.lo@gmail.com>
2018-01-11 14:01:00 -0800

    working thread

--- threads.c
+++ threads.c
@@ -3,49 +3,51 @@
 #include <gmp.h>
 #include "primedecompose.h"
 #include <pthread.h>
+#include <iostream>
+
+using namespace std;
 
 // uses the GMP (GNU Multiple Precision Library for the computations
 //gcc -Wall -o pdec primedec.c primedecompose.c -lgmp
 
 #define MAX_FACTORS	1024
 
-
 void *Start(void *arg);
-// Globals
+
 mpz_t dest[MAX_FACTORS]; // must be large enough to hold all the factors!
 mpz_t n;
 int l;
-const char *value;
 
-int main(int argc, char **argv)
-{
+int main(int argc, char *argv[]){
   int i;
-	pthread_t tid;
-    value = (char*)argv[1];
-	if(argc != 2)
-	{
+  pthread_t tid;
+	if(argc != 2){
 			puts("Usage: ./pdec <number to be factored>");
 			return EXIT_SUCCESS;
 	}
 	//for(i = 1; i <= argc; i++){
-			pthread_create(&tid, NULL, Start,(void*) 1);
+	//cout << &argv[1] << endl;
+	pthread_create(&tid, NULL, Start,(void*) argv[1]);
 	//}
+    pthread_join(tid,NULL);
 
-  for(i=0; i < l; i++){
-    		gmp_printf("%s%Zd", i?" * ":"", dest[i]);
-    		mpz_clear(dest[i]);
+    for(i=0; i < l; i++){
+        gmp_printf("%s%Zd", i?" * ":"", dest[i]);
+    	mpz_clear(dest[i]);
   	}
   	printf("\n");
 
   	return EXIT_SUCCESS;
-}
+
+    }
 
 void *Start(void *arg){
-	int val;
-	val = (int)arg;
-	mpz_init_set_str(n, value[val], 10);
+	char* val;
+	val = (char*)arg;
+	mpz_init_set_str(n, val, 10);
 	//if the base is correct return 0 otherwise returns -1
 	l = decompose(n, dest);
 
 	return NULL;
 }
+
